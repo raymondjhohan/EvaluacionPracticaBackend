3c91b191738167794a0fd9fec609b1e722ccada7
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 22:11:32 -0500

    Persona.js
    Postman Persona

---
+++ Persona.js
@@ -0,0 +1,9 @@
+{
+        "idpersona": "234561",
+        "nombre": "Raymon",
+        "genero": "M",
+        "edad": "28",
+        "identificacion": "DNI",
+        "direccion": "Jiron Teniente Melchor"
+		"telefono": "924675694"
+}
