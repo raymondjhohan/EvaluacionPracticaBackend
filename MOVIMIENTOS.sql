ab3769d37b77c931d2f15025af36ff08fbc9271e
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 20:57:55 -0500

    Delete MOVIMIENTOS.sql

--- MOVIMIENTOS.sql
+++
@@ -1,8 +0,0 @@
-create table MOVIMIENTOS
-(
-  idmovimiento                     CHAR(6) PRIMARY KEY,
-  fecha                            DATE,
-  tipomovimiento                   VARCHAR2(10),
-  valor                            VARCHAR2(8), 
-  saldo                            NUMBER(10)
-);
