aed71b544e91ee826c8960d6ad73b7510e9c8df7
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 20:57:45 -0500

    Delete CUENTA.sql

--- CUENTA.sql
+++
@@ -1,8 +0,0 @@
-create table CUENTA
-(
-  clienteid                        CHAR(6) PRIMARY KEY,
-  numerocuenta                     CHAR(6),
-  tipocuenta                       VARCHAR2(8),
-  saldoInicial                     NUMBER(10), 
-  estado                           CHAR(6)
-);
