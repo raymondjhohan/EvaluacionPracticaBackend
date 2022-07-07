826db9394a721cfd4ba0ded1e2ed7af627c35b06
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 20:37:02 -0500

    Delete Cuenta.sql

--- Cuenta.sql
+++
@@ -1,7 +0,0 @@
-create table CUENTA
-(
-  numerocuenta                 CHAR(6) primary key,
-  tipocuenta                       VARCHAR2(8),
-  saldoInicial                      NUMBER(10), 
-  estado                            CHAR(6)
-);
