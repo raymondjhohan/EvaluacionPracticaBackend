58e11f5ac53d417e119e56a93df3e0d63b272ebc
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 22:10:20 -0500

    Actualizar.sql

---
+++ Actualizar.sql
@@ -0,0 +1,11 @@
+update PERSONA set nombre = 'Raymon Jhohan'
+  where idpersona = 234561;
+  
+update CLIENTE set contraseña = 'nttdata203'
+  where clienteid = 234561;
+  
+update Cuenta set tipocuenta = '585546'
+  where cuentaid = 234561;
+
+update MOVIMIENTOS set valor = '110'
+  where idmovimiento = 234561;
