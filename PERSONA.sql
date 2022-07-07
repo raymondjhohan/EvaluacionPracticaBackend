218af86fe0f4f05f5f96c40c1592b8220815b969
raymondjhohan <46501904+raymondjhohan@users.noreply.github.com>
2022-07-06 20:37:22 -0500

    Delete PERSONA.sql

--- PERSONA.sql
+++
@@ -1,9 +0,0 @@
-create table PERSONA
-(
-  nombre                    VARCHAR2(50) primary key,
-  genero                    CHAR (1) ,
-  edad                      NUMBER(3),
-  identificación            CHAR(3),
-  dirección                 VARCHAR2(100),
-  teléfono                  NUMBER(10)
-);
